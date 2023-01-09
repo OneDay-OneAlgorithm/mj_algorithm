55a8227590710e5587b077a305ca9bca9b689bc5
MJ <80272444+minai621@users.noreply.github.com>
2023-01-09 09:56:35 +0000

    2563

---
+++ boj/javascript/2d_array/2563.js
@@ -0,0 +1,29 @@
+// 5. 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+/* ex)
+3
+110
+78
+158
+*/
+const fs = require('fs');
+const input = fs
+  .readFileSync('./boj/javascript/input.txt')
+  .toString()
+  .trim()
+  .split('\n');
+const N = input[0];
+input.shift();
+let copy = input.map((v) => v.split(' ').map((e) => Number(e)));
+const matrix = new Array(100).fill().map((el) => new Array(100).fill(false));
+
+for (let i = 0; i < N; i++) {
+  const x = copy[i][0];
+  const y = copy[i][1];
+  for (let j = 0; j < 10; j++) {
+    for (let k = 0; k < 10; k++) {
+      matrix[x + j][y + k] = true;
+    }
+  }
+}
+
+console.log(matrix.flat().filter((v) => v).length);
