1b4f3038f48976bac767ff49568b5b246e2e70cf
MJ <80272444+minai621@users.noreply.github.com>
2023-04-17 07:48:27 +0000

    15650

---
+++ boj/javascript/backtracking/15650.js
@@ -0,0 +1,33 @@
+// 2. 입력값이 여러 개일 때(한 줄에 공백으로 구분)
+/* ex)
+110 78 158
+*/
+const fs = require('fs');
+const input = fs
+  .readFileSync('boj/javascript/input.txt')
+  .toString()
+  .trim()
+  .split(' ');
+const [N, M] = input.map((v) => parseInt(v));
+
+const numbers = [];
+const isUsed = new Array(N + 1).fill(false);
+
+function backtracking(idx, depth) {
+  if (depth === M) {
+    console.log(numbers.join(' '));
+    return;
+  }
+
+  for (let i = idx; i < N; i++) {
+    if (!isUsed[i]) {
+      numbers.push(i + 1);
+      isUsed[i] = true;
+      backtracking(i, depth + 1);
+      isUsed[i] = false;
+      numbers.pop();
+    }
+  }
+}
+
+backtracking(0, 0);
