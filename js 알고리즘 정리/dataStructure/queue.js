aa6deff70cbc174ddbe0711b5f6c668b051b5911
MJ <80272444+minai621@users.noreply.github.com>
2023-05-13 06:32:46 +0000

    implement queue

---
+++ js 알고리즘 정리/dataStructure/queue.js
@@ -0,0 +1,37 @@
+export class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return "Queue is empty";
+    }
+    return this.items.shift();
+  }
+
+  front() {
+    if (this.isEmpty()) {
+      return "Queue is empty";
+    }
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  printQueue() {
+    let str = "";
+    for (let i = 0; i < this.items.length; i++) {
+      str += this.items[i] + " ";
+    }
+    return str;
+  }
+}
+
+const queue = new Queue();
