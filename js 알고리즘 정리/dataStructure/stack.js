bb71e09928b926c381301b4f1a58078030c91ef3
MJ <80272444+minai621@users.noreply.github.com>
2023-05-13 06:32:03 +0000

    implement stack

---
+++ js 알고리즘 정리/dataStructure/stack.js
@@ -0,0 +1,34 @@
+export class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.items.length === 0) {
+      return "Stack is empty";
+    }
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  printStack() {
+    let str = "";
+    for (let i = 0; i < this.items.length; i++) {
+      str += this.items[i] + " ";
+    }
+    return str;
+  }
+}
+
+const stack = new Stack();
